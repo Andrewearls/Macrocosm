128f59723e773f21932db0500a0e377ddb89dc7c
Andrew Earls <andrewearls@rocketmail.com>
2018-09-23 15:00:04 -0700

    welcome screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,11 @@
-<style type="text/css">
-    img {
-        width: 100%;
-        height: 100%;
-        margin: 0px;
-        padding:0px;
-    }
-    html {
-        padding: 0px;
-        margin: 0px;
-    }
-    body{
-        padding: 0px;
-        margin: 0px;
-    }
-</style>
-<img src="https://images.unsplash.com/photo-1522124624696-7ea32eb9592c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=decc4d1643fb3710366aa1346573ffc8&auto=format&fit=crop&w=1050&q=80">
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1 id="welcome-font">Welcome Explorer</h1>
+        </div>
+    </div>
+</div>
+@endsection
