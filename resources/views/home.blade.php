c54228b8f75beaaf5486ecd212dc0ae71e0b7803
Andrew Earls <andrewearls@rocketmail.com>
2018-09-24 17:55:08 -0700

    fixed dashboard mobil layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
                                     <div class="title">Shopping</div>
                                     <p>Shop the macrocosmic-store!</p>
                                 </div> 
-                                <div class="col-md-4">
+                                <div class="col-md-4 hidden-sm-down">
                                     <i class="fas fa-shopping-bag fa-5x"></i>
                                 </div>                       
                             </a>
@@ -30,14 +30,14 @@
                                     <div class="title">Badges</div>
                                     <p>View your recent achievements!</p>
                                 </div> 
-                                <div class="col-md-4">
+                                <div class="col-md-4 hidden-sm-down">
                                     <i class="fas fa-shield-alt fa-5x"></i>
                                 </div>                       
                             </a>
                         </div>
                         <div class="col-md-6 right">
                             <a class="row">   
-                                <div class="col-md-4">
+                                <div class="col-md-4 hidden-sm-down">
                                     <i class="fas fa-bullseye fa-5x"></i>
                                 </div>                   
                                 <div class="col-md-8">
@@ -46,7 +46,7 @@
                                 </div>   
                             </a>
                             <a class="row">   
-                                <div class="col-md-4">
+                                <div class="col-md-4 hidden-sm-down">
                                     <i class="fas fa-flag fa-5x"></i>
                                 </div>                   
                                 <div class="col-md-8">
