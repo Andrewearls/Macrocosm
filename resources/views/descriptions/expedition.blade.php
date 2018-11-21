c798954151523c9eea248113524072413459f6c5
Andrew Earls <andrewearls@rocketmail.com>
2018-11-21 15:45:37 -0800

    expedition default description image

--- resources/views/descriptions/expedition.blade.php
+++ resources/views/descriptions/expedition.blade.php
@@ -4,6 +4,19 @@
     id="expedition"
 @endsection
 
+@section('cardbody')
+    <div class="row no-gutters">
+        <div class="col-sm-4 image-container">
+        	<img src="{{ $result->image }}" class="fas fa-flag fa-5x">
+        </div>
+        <div class="col-sm-8 long-description">
+        	
+        	<p>Description: {{ $result->description }}</p>
+            
+        </div>
+    </div>
+@endsection 
+
 @section('cardfooter')
     <div class="row justify-content-between no-gutters">
 	    <a href="{{ route('expeditions') }}" class="col-sm-3 btn">Back to Expeditions</a>
