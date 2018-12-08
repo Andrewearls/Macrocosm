32485900fec469dec727237064ab37efe0e7787f
Andrew Earls <andrewearls@rocketmail.com>
2018-12-08 10:22:07 -0800

    fixed courses index badge icon

--- resources/views/indexes/courses.blade.php
+++ resources/views/indexes/courses.blade.php
@@ -17,4 +17,22 @@
     </div>
 @endsection
 
+@section('cardbody')        
+    <div class="row no-gutters">
+        @foreach((array) $results as $result)
+            <a href="{{ route( $routeName, ['id' => $result['id']]) }}" class="col-6 col-sm-3 item-container {{ $result['class'] }}">
+                
+                <i class="fas fa-bullseye fa-5x"></i>
+                <div class="title">
+                    @if(strlen($result['name']) > 12)
+                        {{ substr($result['name'], 0, 12) }}...
+                    @else
+                        {{ $result['name'] }}
+                    @endif
+                </div>
+            </a>
+        @endforeach
+    </div>        
+@endsection
+
 
