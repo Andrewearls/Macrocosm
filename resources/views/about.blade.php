e9d0daf92bfb7bde34819baed3f368c27fd7fbe6
Andrew Earls <andrewearls@rocketmail.com>
2019-04-29 12:28:51 -0700

    guests can visit about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.card')
+
+@section('layoutid')
+    id="about"
+@endsection
+
+@section('cardheader')
+    <div class="row justify-content-between">
+        <div class="col-12 col-sm-5 title">
+            About
+        </div>
+    </div>
+@endsection
+
+@section('cardbody') 
+    <div class="row no-gutters">
+        <div class="col-md-6 left">
+        	Guest
+        </div>
+    </div>
+@endsection
+    
