4fe3a72a7db49b1f36053191cb4af30652017cdc
Andrew Earls <andrewearls@rocketmail.com>
2019-04-29 12:55:53 -0700

    about page constructed

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,16 +7,49 @@
 @section('cardheader')
     <div class="row justify-content-between">
         <div class="col-12 col-sm-5 title">
-            About
+            About us
         </div>
     </div>
 @endsection
 
 @section('cardbody') 
+	
+    
+	<div class="row no-gutters">
+        <div class="col-md-12 ">
+			<div class="card">
+				<div class="card-header title">
+					Who we are:
+				</div>
+				<div class="card-body">
+					
+				</div>
+			</div>
+        </div>
+    </div>
     <div class="row no-gutters">
-        <div class="col-md-6 left">
-        	Guest
+        <div class="col-md-12 ">
+        	<div class="card">
+
+	        	<div class="card-header title">
+					What we've been up to:
+				</div>
+				<div class="card-body">
+				</div>
+			</div>
+		</div>
+	</div>
+    <div class="row no-gutters">
+        <div class="col-md-12 ">
+			<div class="card">
+				<div class="card-header title">
+					How you can be part of it:
+				</div>
+				<div class="card-body">
+					
+				</div>
+			</div>
         </div>
     </div>
 @endsection
-    
+
